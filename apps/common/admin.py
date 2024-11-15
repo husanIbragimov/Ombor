1fae7f68773c142f0610264805be91ffa7461015
Husan Ibragimov <lazy.devsloper@gmail.com>
2024-11-15 23:05:22 +0500

    Add initial migrations and update admin settings

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Currency
 
+# admin page name
+admin.site.site_header = 'Ombor'
+admin.site.site_title = 'Admin panel'
+admin.site.index_title = 'Ombor'
+
 admin.site.register(Currency)
--- apps/list/models.py
+++ apps/list/models.py
@@ -6,6 +6,7 @@ from apps.product.models import Product
 
 class ListToBuy(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='list_to_buy')
+    name = models.CharField(max_length=255, null=True, blank=True)
     amount = models.PositiveIntegerField(default=1)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     where_to_buy = models.CharField(max_length=255, null=True, blank=True)
